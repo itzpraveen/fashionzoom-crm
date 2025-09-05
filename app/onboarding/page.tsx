5913daf889b047f86dd68a0ca1c05596ac54e903
fashionzoommagazinefz-cmyk <fashionzoommagazinefz-cmyk@users.noreply.github.com>
2025-09-05 11:45:51 +0530

    chore: remove onboarding route; simplify redirects to /leads

--- app/onboarding/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export const dynamic = 'force-dynamic'
-
-export default async function OnboardingPage() {
-  // Onboarding is no longer required; send users to leads
-  redirect('/leads')
-}
